3847aa0146bcc1fe2b79c633e25df46d1f2dc8a4
John Damilola Ogungbure <jogungbure@gmail.com>
2022-05-18 19:12:55 +0100

    switched to using repository

--- src/components/user/daos/index.ts
+++ src/components/user/daos/index.ts
@@ -1,12 +1,19 @@
 import { User } from '../../../db/entities/User'
-import { FindOptionsWhere } from 'typeorm'
 import { ICreateUser, IUpdateUser, IFetchUser } from '../types/dtos'
 import { IUser } from '../types/model'
 
-export const create = async (data: ICreateUser): Promise<IUser> => {
-	const newUser = User.create(data)
-	await newUser.save()
+import BaseRepository from '../../../db/repository/BaseRepository'
 
+const userRepository = new BaseRepository(User)
+
+export const fetchOne = async (query: IFetchUser): Promise<IUser | null> => {
+	const user = await User.findOneBy(query)
+
+	return user
+}
+
+export const create = async (data: ICreateUser): Promise<IUser> => {
+	const newUser = await userRepository.create<ICreateUser, IUser>(data)
 	return newUser
 }
 
@@ -14,14 +21,11 @@ export const update = async (
 	query: IFetchUser,
 	data: IUpdateUser,
 ): Promise<IUser | null> => {
-	const user = await User.findOneBy(query)
-	await user?.save(data)
+	const updatedUser = await userRepository.update<
+		IFetchUser,
+		IUpdateUser,
+		IUser
+	>(query, data)
 
-	return user
-}
-
-export const fetchOne = async (query: IFetchUser): Promise<IUser | null> => {
-	const user = await User.findOneBy(query)
-
-	return user
+	return updatedUser
 }
---
+++ src/db/repository/BaseRepository.ts
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+class BaseRepository {
+	Model: any
+
+	constructor(Model: any) {
+		this.Model = Model
+	}
+
+	async fetch<TQuery, TReturn>(query: TQuery): Promise<TReturn> {
+		const entity = await this.Model.find(query)
+		return entity
+	}
+
+	async fetchOne<TQuery, TReturn>(query: TQuery): Promise<TReturn> {
+		const entity = await this.Model.find(query)
+		return entity
+	}
+
+	async create<TCreate, TReturn>(data: TCreate): Promise<TReturn> {
+		const entity = await this.Model.create(data)
+		await entity.save()
+
+		return entity
+	}
+
+	async update<TQuery, TUpdate, TReturn>(
+		query: TQuery,
+		data: TUpdate,
+	): Promise<TReturn | null> {
+		const entity = await this.Model.findOneBy(query)
+		await entity.save(data)
+
+		return entity
+	}
+
+	async destroy<TQuery>(query: TQuery): Promise<boolean> {
+		this.Model.remove(query)
+
+		return true
+	}
+}
+
+export default BaseRepository
